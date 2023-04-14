9412a65aa3d5a89ba122a845ddcf3a18e0bad809
andy <andrea.tol.flores@gmail.com>
2023-04-13 21:59:26 -0600

    Se modificó absolutamente todo el diseño anterior, se dejó página de inicio con algunas animaciones y un poco de la parte sobre mi.

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".input", {
+    strings: ["Andrea Toledo Flores.", "Creativa.", "Curiosa.", "Detallista.", "Divertida."],
+    typeSpeed: 50,
+    backSpeed: 10,
+    loop: true
+  });
